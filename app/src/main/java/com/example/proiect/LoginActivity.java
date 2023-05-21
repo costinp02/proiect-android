d1263360432eeede76bba3ff3eddd1c76464dcb3
costinp02 <costinpin02@gmail.com>
2023-05-21 18:41:49 +0300

    added login page visual interface

--- app/src/main/java/com/example/proiect/LoginActivity.java
+++ app/src/main/java/com/example/proiect/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.proiect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText userEmail, userPass;
+    private Button submitBttn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+        setVariable();
     }
+
+    private void setVariable(){
+        submitBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(userEmail.getText().toString().isEmpty() || userPass.getText().toString().isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Please input valid info", Toast.LENGTH_SHORT).show();
+                }else{
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        });
+    }
+
+    private void initView(){
+        userEmail = findViewById(R.id.login_edittext_email);
+        userPass = findViewById(R.id.login_edittext_password);
+        submitBttn = findViewById(R.id.login_button);
+    }
+
+
 }
